6f01d6242f2a8d83715f0df0dba7ce28b745a128
GunaVarma11 <gunavarmagv@gmail.com>
2024-01-10 18:20:06 +0530

    initialyse

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string ClassAndSection { get; set; }
+}
+
+
+public class Subject
+{
+    public string Name { get; set; }
+    public string SubjectCode { get; set; }
+    public Teacher Teacher { get; set; }
+}
+
+
+public class Teacher
+{
+    public string Name { get; set; }
+}
